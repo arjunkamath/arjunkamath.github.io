7d2cd8e971c3a06f2587c3c61600b36d4e1991a7
Arjun <junnaonly@gmail.com>
2014-11-04 22:01:14 +0200

    HSL complete

--- controller.js
+++ controller.js
@@ -2,7 +2,6 @@ app.controller('AppController', function($scope, $http) {
 
    var hsl = 'https://www.kimonolabs.com/api/aiidm0q8?apikey=jvGLSJppQ52Xpgelxepq468kRrKNEgli&callback=JSON_CALLBACK';	
    
-   
    $scope.hsltime = [];
    $scope.hslbus = [];
 
@@ -10,5 +9,4 @@ app.controller('AppController', function($scope, $http) {
       $scope.hsltime.push.apply($scope.hsltime, data.results.collection1);
       $scope.hslbus.push.apply($scope.hslbus, data.results.collection2);
     });
-
 });

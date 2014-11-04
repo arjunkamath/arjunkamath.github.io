78fef244a7d67e76c4ee4ecb131bf20a54b8b1e6
Arjun <junnaonly@gmail.com>
2014-11-04 21:25:37 +0200

    something went wrong

--- controller.js
+++ controller.js
@@ -1,72 +1,12 @@
 app.controller('AppController', function($scope, $http) {
 
    var hsl = 'https://www.kimonolabs.com/api/aiidm0q8?apikey=jvGLSJppQ52Xpgelxepq468kRrKNEgli&callback=JSON_CALLBACK';	
-   var fixes_url = 'https://www.kimonolabs.com/api/2o3try8i?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-   var brakes_url = 'https://www.kimonolabs.com/api/e44espis?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-   var seats_url = 'https://www.kimonolabs.com/api/9bt8hm54?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-   var handlebars_url = 'https://www.kimonolabs.com/api/4gzood5e?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-   var bikes_url = 'https://www.kimonolabs.com/api/4ivtj7ei?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-   var frames_url = 'https://www.kimonolabs.com/api/7e2qax1w?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-   var drivetrain_url = 'https://www.kimonolabs.com/api/94v4xqrq?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-   var fillari_lehti_url = 'https://www.kimonolabs.com/api/c2eip02c?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-   var yksivaihde = 'https://www.kimonolabs.com/api/ctxw3wfy?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
-    var tori = 'https://www.kimonolabs.com/api/8lxgfcge?apikey=06009849069bc5807cded9e31b4d029b&callback=JSON_CALLBACK';
    
-   
-   $scope.allresults = [];
-//   $scope.hsltime = [];
+   $scope.hsltime = [];
    $scope.hslbus = [];
 
    $http.jsonp(hsl).success(function(data) {
-     // $scope.allresults.push.apply($scope.hsltime, data.results.collection1);
+      $scope.hsltime.push.apply($scope.hsltime, data.results.collection1);
       $scope.hslbus.push.apply($scope.hslbus, data.results.collection2);
     });
-
-
-   $http.jsonp(frames_url).success(function(data) {
-      $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-   
-    $http.jsonp(bikes_url).success(function(data) {
-      $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-   
-    $http.jsonp(handlebars_url).success(function(data) {
-      $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-   
-    $http.jsonp(yksivaihde).success(function(data) {
-      $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-   $http.jsonp(fixes_url).success(function(data) {
-      $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-    $http.jsonp(brakes_url).success(function(data) {
-      $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-   //$http.jsonp(tori).success(function(data) {
-   //   $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   //});
-    $http.jsonp(seats_url).success(function(data) {
-      $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-   $http.jsonp(drivetrain_url).success(function(data) {
-	 $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-   $http.jsonp(drivetrain_url).success(function(data) {
-	 $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-   $http.jsonp(drivetrain_url).success(function(data) {
-	 $scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-  
-	$http.jsonp(fillari_lehti_url).success(function(data) {
-		data.results.collection1.forEach(function(element) {
-		element.item = { text: element.item, 
-						 href: 'http://www.fillari-lehti.fi/Tori.aspx'
-					   };
-		});
-		$scope.allresults.push.apply($scope.allresults, data.results.collection1);
-   });
-
 });

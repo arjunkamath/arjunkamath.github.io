60a1d68414293d22311009f8632b86429ab5f63f
ARJUN KAMATH <junnaonly@gmail.com>
2014-11-05 14:44:46 +0200

    Antell menu week and day

--- controller.js
+++ controller.js
@@ -2,10 +2,19 @@ app.controller('AppController', function($scope, $http) {
 
    var hsl = 'https://www.kimonolabs.com/api/aiidm0q8?apikey=jvGLSJppQ52Xpgelxepq468kRrKNEgli&callback=JSON_CALLBACK';	
    var espoo_temp = 'https://www.kimonolabs.com/api/6j6i6ot2?apikey=jvGLSJppQ52Xpgelxepq468kRrKNEgli&callback=JSON_CALLBACK';
+   var antell_url = 'https://www.kimonolabs.com/api/8k73atw6?apikey=jvGLSJppQ52Xpgelxepq468kRrKNEgli&callback=JSON_CALLBACK';
+   
+   var date = new Date();
+   var day = date.getDay()
  
    $scope.hsltime = [];
    $scope.hslbus = [];
    $scope.weather = [];
+   $scope.antell_menu = [];
+   $scope.antell_single_day_menu = [];
+   $scope.weekday = [];
+   
+   console.log("ng script:" + day);
 
    $http.jsonp(hsl).success(function(data) {
       $scope.hsltime.push.apply($scope.hsltime, data.results.collection1);
@@ -15,4 +24,50 @@ app.controller('AppController', function($scope, $http) {
    $http.jsonp(espoo_temp).success(function(data) {
       $scope.weather.push.apply($scope.weather, data.results.collection1);
     });
+	
+	$http.jsonp(antell_url).success(function(data) {
+	  var d = new Date();
+	  var n = d.getDay();
+	  
+	  console.log("ng script:" + day);
+	  console.log("2ng script:" + n);
+	  
+	  $scope.antell_menu.push.apply($scope.antell_menu, data.results.collection1); 
+		$scope.antell_menu.push.apply($scope.antell_menu, data.results.collection2); 
+		$scope.antell_menu.push.apply($scope.antell_menu, data.results.collection3); 
+		$scope.antell_menu.push.apply($scope.antell_menu, data.results.collection4); 
+		$scope.antell_menu.push.apply($scope.antell_menu, data.results.collection5); 
+	  
+	  
+		if (n === 1)
+		{
+			console.log("Weekday:" + n);
+			$scope.antell_single_day_menu.push.apply($scope.antell_single_day_menu, data.results.collection1); 
+		}
+		else if (n === 2)
+		{
+			console.log("Weekday:" + n);
+			$scope.antell_single_day_menu.push.apply($scope.antell_single_day_menu, data.results.collection1); 
+		}
+		else if (n === 3)
+		{
+			console.log("Weekday:" + n);
+			$scope.antell_single_day_menu.push.apply($scope.antell_single_day_menu, data.results.collection2); 
+		}
+		else if (n === 4)
+		{
+			console.log("Weekday:" + n);
+			$scope.antell_single_day_menu.push.apply($scope.antell_single_day_menu, data.results.collection4); 
+		}
+		else if (n === 5)
+		{
+			console.log("Weekday:" + n);
+			$scope.antell_single_day_menu.push.apply($scope.antell_single_day_menu, data.results.collection5); 
+		}		
+	  	
+	  
+	  
+    });
 });
+
+

25e91e2a8fed257b05e432dcd541fe88eb3af990
Arjun <junnaonly@gmail.com>
2014-11-18 20:06:30 +0200

    Added weather for 4 cities and images

--- weather.js
+++ weather.js
@@ -5,11 +5,57 @@ $(document).ready(function() {
     woeid: '',
     unit: 'c',
     success: function(weather) {
-      html = weather.temp + '&deg' + weather.units.temp + ' ' +weather.currently;
-      $("#weather").html(html);
+      html = ' <b>@Helsinki</b> ' + weather.temp + '&deg' + weather.units.temp + ' ' + weather.currently;
+      html += '<p><img src="'+ weather.thumbnail+'"></p>'
+      $("#weather_hel").html(html);
     },
     error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
+      $("#weather_hel").html('<p>'+error+'</p>');
     }
   });
+
+  $.simpleWeather({
+    location: 'Kirkkonummi',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      html = '<b>@Kirkkonummi</b> ' + weather.temp + '&deg' + weather.units.temp + ' ' + weather.currently;
+      html += '<p><img src="'+ weather.thumbnail+'"></p>'
+      $("#weather_kik").html(html);
+    },
+    error: function(error) {
+      $("#weather_kik").html('<p>'+error+'</p>');
+    }
+  });
+
+
+  $.simpleWeather({
+    location: 'Espoo',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      html = '<b>@Espoo</b> ' + weather.temp + '&deg' + weather.units.temp + ' ' + weather.currently;
+      html += '<p><img src="'+weather.thumbnail+'"></p>'
+      $("#weather_esp").html(html);
+    },
+    error: function(error) {
+      $("#weather_esp").html('<p>'+error+'</p>');
+    }
+  });
+
+  
+  $.simpleWeather({
+    location: 'Turku',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      html = '<b>@Turku</b> ' + weather.temp + '&deg' + weather.units.temp + ' ' + weather.currently;
+      html += '<p><img src="'+weather.thumbnail+'"></p>'
+      $("#weather_tur").html(html);
+    },
+    error: function(error) {
+      $("#weather_tur").html('<p>'+error+'</p>');
+    }
+  });
+
 });
